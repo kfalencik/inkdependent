ac18e8994c3460ccd6e80d43eae60d67ed937fc6
kfalencik <kevin.falencik@amazerealise.com>
2019-05-06 16:51:31 +0100

    Fix cached files

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,20 +2,20 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('inkdependent').then(function(cache) {
 			return cache.addAll([
-				'/',
-				'/index.html',
-				'/images/logo.png',
-				'/images/logo-small.png',
-				'/images/marker.png',
-				'/styles/main.css',
-				'/scripts/main.min.js',
-				'/images/slideshow/slide-1.jpg',
-				'/images/slideshow/slide-2.jpg',
-				'/images/slideshow/slide-3.jpg',
-				'/images/slideshow/slide-4.jpg',
-				'/images/graphics/about-us.jpg',
-				'/images/graphics/social.jpg',
-				'favicon.ico'
+				'./',
+				'./index.html',
+				'./images/logo.png',
+				'./images/logo-small.png',
+				'./images/marker.png',
+				'./styles/main.css',
+				'./scripts/main.js',
+				'./images/slideshow/slide-1.jpg',
+				'./images/slideshow/slide-2.jpg',
+				'./images/slideshow/slide-3.jpg',
+				'./images/slideshow/slide-4.jpg',
+				'./images/graphics/about-us.jpg',
+				'./images/graphics/social.jpg',
+				'./favicon.ico'
 			]);
 		})
 	);
