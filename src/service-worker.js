5a9cf04cb6eda49eea317022fa6bc1d8bc0b0f0e
kfalencik <kevin.falencik@amazerealise.com>
2019-05-06 16:53:46 +0100

    HOTFIX

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,19 +3,7 @@ self.addEventListener('install', function(event) {
 		caches.open('inkdependent').then(function(cache) {
 			return cache.addAll([
 				'./',
-				'./index.html',
-				'./images/logo.png',
-				'./images/logo-small.png',
-				'./images/marker.png',
-				'./styles/main.css',
-				'./scripts/main.js',
-				'./images/slideshow/slide-1.jpg',
-				'./images/slideshow/slide-2.jpg',
-				'./images/slideshow/slide-3.jpg',
-				'./images/slideshow/slide-4.jpg',
-				'./images/graphics/about-us.jpg',
-				'./images/graphics/social.jpg',
-				'./favicon.ico'
+				'./index.html'
 			]);
 		})
 	);
