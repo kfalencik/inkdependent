2c331cdafc5bfe56dd4017c858b21802ee8e217a
kfalencik <kevin.falencik@amazerealise.com>
2019-05-06 17:10:50 +0100

    Move scripts to footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -59,11 +59,5 @@ map.scrollZoom.disable();
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/service-worker.js', {
 		updateViaCache: 'none'
-	})
-	.then(function(registration) {
-		console.log('Registration successful, scope is:', registration.scope);
-	})
-	.catch(function(error) {
-		console.log('Service worker registration failed, error:', error);
 	});
 }
