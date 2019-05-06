cad7e4f23e949bfb61451ace70d76bd99245657b
kfalencik <kevin.falencik@amazerealise.com>
2019-05-06 16:38:00 +0100

    Offline service worker finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -56,3 +56,12 @@ map.on('load', function () {
 
 map.scrollZoom.disable();
 
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/service-worker.js')
+	.then(function(registration) {
+		console.log('Registration successful, scope is:', registration.scope);
+	})
+	.catch(function(error) {
+		console.log('Service worker registration failed, error:', error);
+	});
+}
